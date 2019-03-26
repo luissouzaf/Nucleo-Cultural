ff49746c118737257d6694b8f38166a61f7c16fc
Luís Fernando Souza <luis.souzaf@hotmail.com>
2019-03-26 11:08:46 -0300

    integração Luana-Luis

--- js/script.js
+++ js/script.js
@@ -6,6 +6,29 @@ $(document).ready(function () {
 });	
 
 $(document).ready(function () {
+  var $element = $('#O-Nucleo');
+  var $follow = $element.find('.title-aside-wrapper');
+  var followHeight = $element.find('.title-aside-wrapper').outerHeight();
+  var height = $element.outerHeight();
+  var window_height = $(window).height();
+
+  $(window).scroll(function () {
+    var pos = $(window).scrollTop();
+    var top = $element.offset().top;
+
+
+    if (top + height - followHeight < pos + (window_height / 2 - followHeight / 2) || top > pos + (window_height / 2 - followHeight / 2) ) { 
+    	return; 
+    } 
+    var offset = parseInt($(window).scrollTop() - top + (window_height / 2 - followHeight / 2));
+    
+    if (offset > 0) {
+    $follow.css('transform', 'translateY('+ offset +'px)');
+    }
+  });
+});
+
+$(document).ready(function () {
   var $element = $('#servicos');
   var $follow = $element.find('.title-aside-wrapper');
   var followHeight = $element.find('.title-aside-wrapper').outerHeight();
@@ -56,6 +79,29 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   var $element = $('#parceiros');
+  var $follow = $element.find('.title-aside-wrapper');
+  var followHeight = $element.find('.title-aside-wrapper').outerHeight();
+  var height = $element.outerHeight();
+  var window_height = $(window).height();
+
+  $(window).scroll(function () {
+    var pos = $(window).scrollTop();
+    var top = $element.offset().top;
+
+
+    if (top + height - followHeight < pos + (window_height / 2 - followHeight / 2) || top > pos + (window_height / 2 - followHeight / 2) ) { 
+    	return; 
+    } 
+    var offset = parseInt($(window).scrollTop() - top + (window_height / 2 - followHeight / 2));
+    
+    if (offset > 0) {
+    $follow.css('transform', 'translateY('+ offset +'px)');
+    }
+	});
+});
+
+$(document).ready(function () {
+  var $element = $('#contato');
   var $follow = $element.find('.title-aside-wrapper');
   var followHeight = $element.find('.title-aside-wrapper').outerHeight();
   var height = $element.outerHeight();
